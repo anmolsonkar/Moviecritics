b5284186c330fa94d9188374e0e9b54b8e0d0419
AnmolSonkar <anmolsonkar742@gmail.com>
2024-03-13 01:33:02 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -21,8 +21,6 @@ const URI = process.env.MONGODB_URI;
 
 app.use('/api', routes);
 
-const port = process.env.PORT || 4000;
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(4000, () => {
+    console.log('Server is listening on port 4000');
 });
