cada1334a798bba355e27c40848a817b3a412f18
AnmolSonkar <anmolsonkar742@gmail.com>
2024-04-15 17:03:20 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -24,27 +24,6 @@ app.use('/api', routes);
 
 const port = process.env.PORT || 4000;
 
-// Wrap the app.listen in pm2.connect to manage it with PM2
-pm2.connect(function (err) {
-    if (err) {
-        console.error(err);
-        process.exit(2);
-    }
-
-    pm2.start({
-        name: 'moviecritics',
-        script: 'server.js',
-        autorestart: true,
-        watch: true,
-        ignore_watch: ["node_modules", "logs"],
-        max_memory_restart: '1G',
-        env: {
-            "PORT": port,
-            "NODE_ENV": "production",
-        },
-    }, function (err, apps) {
-        pm2.disconnect();
-        if (err) throw err;
-        console.log('PM2 started successfully');
-    });
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
