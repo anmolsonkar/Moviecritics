19b8860ecc53571c2d840968c670a4b624c1dcdc
AnmolSonkar <anmolsonkar742@gmail.com>
2024-04-15 16:44:56 +0530

    pm2 added

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import routes from './routes/routers.js';
 import 'dotenv/config';
+import pm2 from 'pm2';
 
 const app = express();
 app.use(express.json());
@@ -23,6 +24,27 @@ app.use('/api', routes);
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// Wrap the app.listen in pm2.connect to manage it with PM2
+pm2.connect(function (err) {
+    if (err) {
+        console.error(err);
+        process.exit(2);
+    }
+
+    pm2.start({
+        name: 'moviecritics',
+        script: 'server.js',
+        autorestart: true,
+        watch: true,
+        ignore_watch: ["node_modules", "logs"],
+        max_memory_restart: '1G',
+        env: {
+            "PORT": port,
+            "NODE_ENV": "production",
+        },
+    }, function (err, apps) {
+        pm2.disconnect();
+        if (err) throw err;
+        console.log('PM2 started successfully');
+    });
 });
